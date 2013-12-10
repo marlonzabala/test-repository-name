aca8fa8f8400fb5fe354a9a1371e4bc33dbe0dc6
ITCOREA-DEV <ITCOREA-DEV@ITCOREA-DEV-PC>
2013-12-10 16:26:35 +0800

    tester

---
+++ TestPageView/src/com/example/testpageview/MenuDrawerActivity.java
@@ -0,0 +1,25 @@
+package com.example.testpageview;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MenuDrawerActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_menu_drawer);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_drawer, menu);
+		return true;
+	}
+
+}
